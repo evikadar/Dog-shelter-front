3c210386b574cf623ee7ae0a41189e35f4e4bcac
Adam Gonda <adamgondagyula@gmail.com>
2019-03-12 13:25:14 +0100

    dog details component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import DogDetails from './components/DogDetailsPage/DogDetails';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello world
+        <DogDetails />
       </div>
     );
   }
