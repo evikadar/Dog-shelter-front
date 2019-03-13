57d7d9e235d19bb12cbb0320db1e5f1645210a7d
Halmai Mirella <halmaimirella@gmail.com>
2019-03-13 22:23:19 +0100

    gender instead of isBoy and boy

--- src/components/DogDetailsPage/InfoBox.js
+++ src/components/DogDetailsPage/InfoBox.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export default props => {
-        const {breed, age, boy, size, neutered, shelter} = props.details
+        const {breed, age, gender, size, neutered, shelter} = props.details
         const ERROR_MSG = 'no data'
 
         return (
             <div style={props.style}>
                     <p>Breed: {breed ? breed : ERROR_MSG}</p>
                     <p>Age: {age ? age : ERROR_MSG}</p>
-                    <p>Sex: {boy ? setSex(boy) : ERROR_MSG}</p>
+                    <p>Sex: {gender ? setSex(gender) : ERROR_MSG}</p>
                     <p>Size: {size ? size : ERROR_MSG}</p>
                     <p>Neutered: {neutered ? setNeutered(neutered) : ERROR_MSG}</p>
                     <p>Shelter: {shelter ? shelter.name : ERROR_MSG}</p>
@@ -16,8 +16,8 @@ export default props => {
         )
 }
 
-const setSex = isBoy => {
-    return isBoy ? 'Boy' : "Girl"
+const setSex = gender => {
+    return gender === "FEMALE" ? "Female" : "Male"
 }
 
 const setNeutered = neutered => {
