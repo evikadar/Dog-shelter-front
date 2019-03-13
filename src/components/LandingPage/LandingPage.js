528fccc0a67e520b60e0b8e4a667baa39175e0e3
Nora Filep <filepnora@gmail.com>
2019-03-13 17:12:16 +0100

    three dogs appear at the bottom of the main page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -18,36 +18,43 @@ class LandingPage extends React.Component {
                         <h1 id="maintitle">Dog Shelter Project</h1>
                         <div id="subtitle">Find your dream dog!</div>
                         <button id="all-dogs-main-page-button"><NavLink exact activeClassName='active' to='/dogs'>Looking
-                        for a
-                        home</NavLink>
-                    </button>
+                            for a
+                            home</NavLink>
+                        </button>
                     </div>
                     <div>
-
                         {/*<button><NavLink exact activeClassName='active' to='/dog'>dog</NavLink></button>*/}
                     </div>
                 </div>
-                <div>
+
+                <div className="container">
                     <h2 id="example-dogs-text">Added recently:</h2>
-                    {this.makeRandomDogs()}
+                </div>
+                <div className="card-columns">
+                    {this.makeRandomDogs(0)}
+                    {this.makeRandomDogs(1)}
+                    {this.makeRandomDogs(2)}
                 </div>
             </div>
         );
     }
 
-    makeRandomDogs() {
-        console.log(this.state.data[5]);
+    makeRandomDogs(dogNumber) {
         return <div className="DogList">
-            <div className="card w-100">
-                {/*<img className="card-img-top" src={this.state.data[5].photoPath} alt="Card image cap"/>*/}
-                <div className="card-body">
-                    {/*<h4 className="card-title">{this.state.data[5].name}</h4>
-                    <p className="card-text">Age: {this.state.data[5].age}</p>
-                    <p className="card-text">Breed: {this.state.data[5].breed}</p>*/}
+            {this.state.data.length > 0 ? (
+                <div className="card w-100">
+                    <img className="example-dog-img"
+                         src={'http://localhost:8080/img/' + this.state.data[dogNumber].photoPath}
+                         alt="Card image cap"/>
+                    <div className="card-body">
+                        <h4 className="card-title">{this.state.data[dogNumber].name}</h4>
+                        <p className="card-text">Age: {this.state.data[dogNumber].age}</p>
+                        {/*<p className="card-text">Breed: {this.state.data[0].breed}</p>*/}
 
-                    <a href="#" className="btn btn-primary">See more</a>
-                </div>
-            </div>
+                        <a href="#" className="btn btn-primary">See more</a>
+                    </div>
+                </div>) : (<div>Loading dog ...</div>)
+            }
         </div>;
     }
 
