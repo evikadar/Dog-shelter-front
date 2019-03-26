0faa9ff7e746358396e27cda5256f59c3f34a41f
Adam Gonda <adamgondagyula@gmail.comls>
2019-03-26 13:45:46 +0100

    a whole bunch of changes from last week before the demo

--- src/components/DogDetailsPage/styles.js
+++ src/components/DogDetailsPage/styles.js
@@ -1,34 +1,33 @@
 export const mainContainer = {
-    padding: '100px 150px 50px 150px',
-}
+  padding: '100px 150px 50px 150px'
+};
 
 export const infoBox = {
-    padding: 10,
-    marginTop: 20,
-    fontSize: 25,
-
-}
+  padding: 10,
+  marginTop: 20,
+  fontSize: 25
+};
 
 export const description = {
-    padding: 10,
-    marginTop: 50
-}
+  padding: 10,
+  marginTop: 50
+};
 
 export const leftSide = width => ({
-    width: width,
-    float: 'left',
-    fontSize: 25,
-    marginBottom: 100
-})
+  width: width,
+  float: 'left',
+  fontSize: 25,
+  marginBottom: 100
+});
 
 export const rightSide = {
-    float: 'right'
-}
+  float: 'right'
+};
 
 export const btn = width => ({
-    marginTop: 20,
-    display: 'block',
-    padding: 20,
-    fontSize: 50,
-    width: width,
-})
+  marginTop: 20,
+  display: 'block',
+  padding: 20,
+  fontSize: 50,
+  width: width
+});
--- src/components/ShelterIndex/ShelterIndex.js
+++ src/components/ShelterIndex/ShelterIndex.js
@@ -2,84 +2,91 @@ import React from 'react'
 import './ShelterIndex.css'
 
 function Title(props) {
-    return <h1 className='title'>{props.value}</h1>
+  return <h1 className="title">{props.value}</h1>
 }
 
 class ShelterIndex extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoaded: false,
-            dogData: []
-        };
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoaded: false,
+      dogData: []
     }
+  }
 
-    componentDidMount() {
-        fetch("http://localhost:8080/shelter/index", {
-            method: "get",
+  componentDidMount() {
+    fetch('http://localhost:8080/shelter/index', {
+      method: 'get'
+    })
+      .then(response => response.json())
+      .then(result =>
+        this.setState({
+          isLoaded: true,
+          dogData: result
         })
-            .then(response => response.json())
-            .then((result) => this.setState({
-                isLoaded: true,
-                dogData: result
-            }))
-            .catch(function (error) {
-            alert(`Error: ${error}\nIf you see this, our testers did a sloppy job, and our developers an even sloppier`)
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <Title value={this.props.value}/>
-                {this.renderDogBoxes()}
-            </div>
+      )
+      .catch(function(error) {
+        alert(
+          `Error: ${error}\nIf you see this, our testers did a sloppy job, and our developers an even sloppier`
         )
-    }
+      })
+  }
 
-    renderDogBoxes() {
-        return (
-            <div className="card-columns">
-                {this.state.dogData.length > 0 ? (
-                    this.renderAllBoxes()
-                    ) : (
-                        <div>Your dogs are loading...</div>
-                )
-                }
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Title value={this.props.value} />
+        {this.renderDogBoxes()}
+      </div>
+    )
+  }
 
-    renderAllBoxes() {
-        let dogBoxes = [];
-        for (let i = 0; i < this.state.dogData.length; i++) {
-            let dog = this.state.dogData[i];
-            dogBoxes.push(<div>
-                <DogBox dogName={dog.name} dogStatus={dog.status} dogImage={dog.photoPath}/>
-            </div>)
-        }
-        return dogBoxes;
+  renderDogBoxes() {
+    return (
+      <div className="card-columns">
+        {this.state.dogData.length > 0 ? (
+          this.renderAllBoxes()
+        ) : (
+          <div>Your dogs are loading...</div>
+        )}
+      </div>
+    )
+  }
+
+  renderAllBoxes() {
+    let dogBoxes = []
+    for (let i = 0; i < this.state.dogData.length; i++) {
+      let dog = this.state.dogData[i]
+      dogBoxes.push(
+        <div>
+          <DogBox
+            dogName={dog.name}
+            dogStatus={dog.status}
+            dogImage={dog.photoPath}
+          />
+        </div>
+      )
     }
+    return dogBoxes
+  }
 }
 
 class DogBox extends React.Component {
-
-    render() {
-        return (
-            <div className="card w-100">
-                <img className="shelterDogImage card-img-right" src={"http://localhost:8080/img/" + this.props.dogImage} alt="dog"/>
-                <div className="card-body">
-                    <h3 class="dogBox dogName">
-                        {this.props.dogName}
-                    </h3>
-                    <div>
-                        Status: {this.props.dogStatus}
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="card w-100">
+        <img
+          className="shelterDogImage card-img-right"
+          src={'http://localhost:8080/img/' + this.props.dogImage}
+          alt="dog"
+        />
+        <div className="card-body">
+          <h3 class="dogBox dogName">{this.props.dogName}</h3>
+          <div>Status: {this.props.dogStatus}</div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default ShelterIndex;
+export default ShelterIndex
