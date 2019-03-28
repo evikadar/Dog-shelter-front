2ade051c117ca3eeb14ba7e771c6d0e44d2e4a7d
Nora Filep <filepnora@gmail.com>
2019-03-28 11:37:14 +0100

    form filled with current data from database

--- src/components/ShelterPage/ShelterEdit.js
+++ src/components/ShelterPage/ShelterEdit.js
@@ -2,6 +2,26 @@ import React from "react";
 
 class ShelterEdit extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            shelterData: []
+        };
+    }
+
+    componentDidMount() {
+        fetch(`http://localhost:8080/shelter/${this.props.match.params.id}`)
+            .then(response => response.json())
+            .then((result) => {
+                console.log(result);
+                this.setState({
+                    isLoaded: true,
+                    shelterData: result
+                })
+            });
+    }
+
     render() {
         return (
             <div className="container">
@@ -15,84 +35,91 @@ class ShelterEdit extends React.Component {
     }
 
     getForm() {
-        return (
-            <div>
-                <form>
-                    <div className="card m-3">
-                        <h3 className="text-center m-3">General and contact data:</h3>
-                        <div className="row mt-0 ml-3 mr-3 mb-3">
-                            <div className="col m-2">
-                                <label htmlFor="shelter-name">Shelter name:</label>
-                                <input type="text" className="form-control" id="shelter-name"
-                                       placeholder="Example input"/>
+        let shelterInfo = [];
+        for (let i = 0; i < this.state.shelterData.length; i++) {
+            let shelter = this.state.shelterData[i];
+            shelterInfo.push(
+                <div>
+                    <form>
+                        <h2 className="text-center m-3">Change your shelter's profile data</h2>
+                        <div className="card m-3">
+                            <h3 className="text-center m-3">General and contact data:</h3>
+                            <div className="row mt-0 ml-3 mr-3 mb-3">
+                                <div className="col m-2">
+                                    <label htmlFor="shelter-name">Shelter name:</label>
+                                    <input type="text" className="form-control" id="shelter-name"
+                                           defaultValue={shelter.name}/>
+                                </div>
+                                <div className="col m-2">
+                                    <label htmlFor="shelter-logo-file">Upload shelter logo:</label>
+                                    <input type="file" className="form-control-file" id="shelter-logo-file"/>
+                                </div>
                             </div>
-                            <div className="col m-2">
-                                <label htmlFor="shelter-logo-file">Upload shelter logo:</label>
-                                <input type="file" className="form-control-file" id="shelter-logo-file"/>
-                            </div>
-                        </div>
-                        <div className="row mt-0 ml-3 mr-3 mb-3">
-                            <div className="col m-2">
-                                <label htmlFor="shelter-email">E-mail:</label>
-                                <input type="text" className="form-control" id="shelter-email"
-                                       placeholder="Example input"/>
-                            </div>
-                            <div className="col m-2">
-                                <label htmlFor="shelter-phone">Phone number:</label>
-                                <input type="text" className="form-control" id="shelter-phone"
-                                       placeholder="Example input"/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card m-3">
-                        <h3 className="text-center m-3">Address:</h3>
-                        <div className="row mt-0 ml-3 mr-3 mb-3">
-                            <div className="col m-3">
-                                <label htmlFor="shelter-address-country">Country:</label>
-                                <input type="text" className="form-control" id="shelter-address-country"
-                                       placeholder="Example input"/>
-                            </div>
-                            <div className="col m-3">
-                                <label htmlFor="shelter-address-city">City:</label>
-                                <input type="text" className="form-control" id="shelter-address-city"
-                                       placeholder="Example input"/>
+                            <div className="row mt-0 ml-3 mr-3 mb-3">
+                                <div className="col m-2">
+                                    <label htmlFor="shelter-email">E-mail:</label>
+                                    <input type="text" className="form-control" id="shelter-email"
+                                           placeholder="Example input" defaultValue={shelter.email}/>
+                                </div>
+                                <div className="col m-2">
+                                    <label htmlFor="shelter-phone">Phone number:</label>
+                                    <input type="text" className="form-control" id="shelter-phone"
+                                           defaultValue={shelter.phoneNumber}/>
+                                </div>
                             </div>
                         </div>
-                        <div className="row mt-0 ml-3 mr-3 mb-3">
-                            <div className="col m-3">
-                                <label htmlFor="shelter-address-address">Address (street, house number):</label>
-                                <input type="text" className="form-control" id="shelter-address-address"
-                                       placeholder="Example input"/>
+                        <div className="card m-3">
+                            <h3 className="text-center m-3">Address:</h3>
+                            <div className="row mt-0 ml-3 mr-3 mb-3">
+                                <div className="col m-3">
+                                    <label htmlFor="shelter-address-country">Country:</label>
+                                    <input type="text" className="form-control" id="shelter-address-country"
+                                           defaultValue={shelter.address.country}/>
+                                </div>
+                                <div className="col m-3">
+                                    <label htmlFor="shelter-address-city">City:</label>
+                                    <input type="text" className="form-control" id="shelter-address-city"
+                                           defaultValue={shelter.address.city}/>
+                                </div>
                             </div>
-                            <div className="col m-3">
-                                <label htmlFor="shelter-address-zip">Zip-code:</label>
-                                <input type="text" className="form-control" id="shelter-address-zip"
-                                       placeholder="Example input"/>
+                            <div className="row mt-0 ml-3 mr-3 mb-3">
+                                <div className="col m-3">
+                                    <label htmlFor="shelter-address-address">Address (street, house number):</label>
+                                    <input type="text" className="form-control" id="shelter-address-address"
+                                           defaultValue={shelter.address.address}/>
+                                </div>
+                                <div className="col m-3">
+                                    <label htmlFor="shelter-address-zip">Zip-code:</label>
+                                    <input type="text" className="form-control" id="shelter-address-zip"
+                                           defaultValue={shelter.address.zipCode}/>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="card m-3">
-                        <h3 className="text-center m-3">Description:</h3>
-                        <div className="row mt-0 ml-3 mr-3 mb-3">
-                            <div className="col m-3">
-                                <label htmlFor="shelter-description">Shelter description (motto, about the adoption-process, etc. ... ): </label>
-                                <textarea className="form-control" id="shelter-description"
-                                          rows="3"></textarea>
+                        <div className="card m-3">
+                            <h3 className="text-center m-3">Description:</h3>
+                            <div className="row mt-0 ml-3 mr-3 mb-3">
+                                <div className="col m-3">
+                                    <label htmlFor="shelter-description">Shelter description (motto, about the
+                                        adoption-process, etc. ... ): </label>
+                                    <textarea className="form-control" id="shelter-description"
+                                              rows="5" defaultValue={shelter.shelterDescription}/>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                </form>
-                <div className="row m-3">
-                    <div className="col text-right">
-                        <button type="cancel" className="btn btn-light" value="Cancel">Cancel</button>
-                    </div>
-                    <div className="col text-left">
-                        <button type="submit" className="btn btn-success" value="Submit">Save changes</button>
+                    </form>
+                    <div className="row m-3">
+                        <div className="col text-right">
+                            <button type="cancel" className="btn btn-light" value="Cancel">Cancel</button>
+                        </div>
+                        <div className="col text-left">
+                            <button type="submit" className="btn btn-success" value="Submit">Save changes</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
+            )
+        }
+        return shelterInfo;
     }
 }
 
