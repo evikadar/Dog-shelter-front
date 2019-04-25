1be4eff0498f301eb059221b4871a59e207f8a84
Halmai Mirella <halmaimirella@gmail.com>
2019-04-25 13:43:44 +0200

    data to fill fields is received from backend

--- src/components/ShelterDogDetailsPage/DogData.js
+++ src/components/ShelterDogDetailsPage/DogData.js
@@ -2,39 +2,26 @@ import * as React from "react";
 
 class DogData extends React.Component {
 
-    constructor(props) {
-        super(props);
-        let dogData = this.props.dogData;
-        this.state = {
-            name: dogData && dogData.name ? dogData.name : "Name",
-            breed: dogData && dogData.breed ? dogData.breed : "Breed",
-            isNeutered: dogData ? (dogData.isNeutered ? "Neutered / Spayed" : "Not Neutered / Spayed") : "Neutered info",
-            dateOfBirth: dogData && dogData.dateOfBirth ? dogData.dateOfBirth : "Date of birth",
-            size: dogData && dogData.size ? dogData.size : "Size",
-            gender: dogData && dogData.gender ? dogData.gender : "Gender",
-            status: dogData && dogData.status ? dogData.status : "Status",
-            personalityTrait: dogData && dogData.description ? dogData.description.personalityTrait : "Personality trait",
-            dreamHome: dogData && dogData.description ? dogData.description.dreamHome : "Dream home",
-            specialFeatures: dogData && dogData.description ? dogData.description.specialFeatures : "Special features",
-        }
-    }
-
     render() {
+        let dogData = this.props.dogData;
         return (
-            <div className={this.props.className}>
-                <div>
-                    {this.state.name}
-                </div>
-                <div>
-                    {this.state.breed} {this.state.gender} {this.state.isNeutered} {this.state.size}
-                </div>
-                <div>
-                    {this.state.status}
-                </div>
-                <div>
-                    {this.state.dateOfBirth}
-                </div>
-            </div>
+            dogData ? (
+                    <div className={this.props.className}>
+                        <div className="h3 card-title">
+                            {dogData.name}
+                        </div>
+                        <div>
+                            {dogData.breed} {dogData.gender} {dogData.isNeutered ? "Neutered / Spayed" : "Not Neutered / Spayed"} {dogData.size}
+                        </div>
+                        <div>
+                            <h4>Status</h4>
+                            {dogData.status}
+                        </div>
+                        <div>
+                            <h4>Date of Birth</h4>
+                            {dogData.dateOfBirth}
+                        </div>
+                    </div>) : null
         );
     }
 
--- src/components/ShelterDogDetailsPage/ShelterDogDetailsPage.js
+++ src/components/ShelterDogDetailsPage/ShelterDogDetailsPage.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import DogData from "./DogData";
 import OwnerInfo from "./OwnerInfo";
 import DogImage from "./DogImage";
+import DogDescription from "./DogDescription";
 
 class ShelterDogDetailsPage extends React.Component {
 
@@ -10,26 +11,59 @@ class ShelterDogDetailsPage extends React.Component {
         this.state = {
             isLoaded: false,
             shelterId: props.match.params.id,
+            dogId: props.match.params.dogId,
             dogData: null,
+            dogDescription: null,
             ownerInfo: null,
             imagePath: ""
         }
     }
 
+    componentDidMount() {
+        fetch(`http://localhost:8080/shelter/${this.state.shelterId}/dog/${this.state.dogId}`)
+            .then(response => response.json())
+            .then((result) => {
+                console.log(result);
+                this.setState(
+                    {
+                        isLoaded: true,
+                        dogData: result,
+                        dogDescription: result.description,
+                        ownerInfo: result.owner,
+                        imagePath: result.photoPath
+                    }
+                )
+            }).catch(function (err) {
+            console.log(err);
+        })
+    }
+
     render() {
+        //todo: make card component!!!
         return (
             <div className="container mt-4">
                 <div className="row">
                     <div className="col-12 col-md-8">
-                        <DogData className="card" dogData={this.state.dogData}/>
+                        <div className="card">
+                            <DogData className="card-body" dogData={this.state.dogData}/>
+                        </div>
                     </div>
                     <div className="col-6 col-md-4">
-                        <DogImage className="card" imageClassName="card-img"
-                                  imagePath={this.state.imagePath.length > 0 ? "http://localhost:8080/image/" + this.state.imagePath : "/dummyDogImage.jpg"}/>
+                        <div className="card-body">
+                            <DogImage className="card" imageClassName="card-img"
+                                      imagePath={this.state.imagePath.length > 0 ? "http://localhost:8080/image/" + this.state.imagePath : "/dummyDogImage.jpg"}/>
+                        </div>
                     </div>
                 </div>
                 <div className="row">
-                    <OwnerInfo className="card" ownerInfo={this.state.ownerInfo}/>
+                    <div className="card">
+                        <OwnerInfo className="card-body" ownerInfo={this.state.ownerInfo}/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="card">
+                        <DogDescription className="card-body" dogDescription={this.state.dogDescription}/>
+                    </div>
                 </div>
             </div>
         )
